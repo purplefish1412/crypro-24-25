bc4a75e1c02cba9db7c6337da7b0a779dbe35844
Denys <denyslapt-ipt27@lll.kpi.ua>
2024-10-08 19:53:21 +0300

    first done

--- lab1/laptiev_fb-22_proskurnia_fb-22_cp1/lab1_main.py
+++ lab1/laptiev_fb-22_proskurnia_fb-22_cp1/lab1_main.py
@@ -4,14 +4,22 @@ def read_file(f_name):
     file = open(f_name, "r", encoding="utf-8")
     return file.read()
 
-def normalize_text_with_space(text):
-    allowed_chars = set("абвгдеёжзийклмнопрстуфхцчшщъыьэюя " + "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ ")
+def normalize_text(text, space, file=False):
+    if (space==True):
+        allowed_chars = set("абвгдеёжзийклмнопрстуфхцчшщъыьэюя " + "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ ")
+    else:
+        allowed_chars = set("абвгдеёжзийклмнопрстуфхцчшщъыьэюя" + "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ")
     norm_text = ''.join(filter(lambda char: char in allowed_chars, text))
     norm_text = norm_text.lower()
     norm_text = norm_text.replace('ъ', 'ь')
     norm_text = norm_text.replace('ё', 'е')
-    with open("normalized_text_with_space.txt", "w", encoding="utf-8") as file:
-        file.write(norm_text)
+    if (file == True and space==True):
+        with open("normalized_text_with_space.txt", "w", encoding="utf-8") as file:
+            file.write(norm_text)
+    if (file == True and space==False):
+        with open("normalized_text_without_space.txt", "w", encoding="utf-8") as file:
+            file.write(norm_text)
+    return norm_text
 
 def count_letters(text):
     letter_count = {}
@@ -35,12 +43,24 @@ def count_bigrams(text):
 
 
 def main():
-    # text = read_file("text.txt")
-    # norm_text = normalize_text_with_space(text)
+    text = read_file("text.txt")
+    norm_text_s = normalize_text(text, True, True)
+    norm_text = normalize_text(text, False, True)
 
-    norm_text = read_file("normalized_text_with_space.txt")
+
+
+    # norm_text_s = read_file("normalized_text_with_space.txt")
+    # norm_text = read_file("normalized_text_without_space.txt")
+
     print(count_letters(norm_text)) 
-    print(len(count_letters(norm_text))) # 32
+    # print(len(count_letters(norm_text))) # 32
+    print('---------------------------------------')
+    print(count_letters(norm_text_s)) 
+    print('---------------------------------------')
+
+    print(count_bigrams(norm_text))
+    print('---------------------------------------')
+    print(count_bigrams(norm_text_s))
 
 
 if __name__ == "__main__":
