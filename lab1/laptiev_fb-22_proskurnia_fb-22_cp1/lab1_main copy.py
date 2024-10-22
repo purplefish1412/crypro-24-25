1841c1d33025ea2f967119a75c5414921ba281bf
Denys <denyslapt-ipt27@lll.kpi.ua>
2024-10-22 21:35:12 +0300

    done(?) lab1_main copy.py

--- lab1/laptiev_fb-22_proskurnia_fb-22_cp1/lab1_main copy.py
+++ lab1/laptiev_fb-22_proskurnia_fb-22_cp1/lab1_main copy.py
@@ -7,10 +7,11 @@ class entrho_calc:
 
     monogram_count = 0
     monograms = {}
-    mono_freq = {}
+    monogram_frequancy = {}
 
     bigram_count = 0
     bigrams = {}
+    bigrams_frequancy = {}
 
     totalOverlappedBigrams = 0
     overlappedBigramCount = {}
@@ -20,17 +21,19 @@ class entrho_calc:
         file = open(path, "r", encoding="utf-8")
         self.text = file.read()
 
-        if (self.space==True):
-            allowed_chars = set("абвгдеёжзийклмнопрстуфхцчшщъыьэюя " + "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ ")
-        else:
-            allowed_chars = set("абвгдеёжзийклмнопрстуфхцчшщъыьэюя" + "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ")
-        self.text = ''.join(filter(lambda char: char in allowed_chars, self.text))
         self.text = self.text.lower()
+        if (self.space==True):
+            allowed_chars = set("абвгдеёжзийклмнопрстуфхцчшщъыьэюя ")
+        else:
+            allowed_chars = set("абвгдеёжзийклмнопрстуфхцчшщъыьэюя")
+        self.text = ''.join(filter(lambda char: char in allowed_chars, self.text))
         self.text = self.text.replace('ъ', 'ь')
         self.text = self.text.replace('ё', 'е')
 
-        # with open("normalized_text.txt", "w", encoding="utf-8") as file:
-        #     file.write(self.text)
+    def save_to_file(self, name=""):
+        with open("normalized_text" + name + ".txt", "w", encoding="utf-8") as file:
+            file.write(self.text)
+            file.close()
 
     def count_monograms(self):
         for char in self.text:
@@ -49,25 +52,58 @@ class entrho_calc:
                 self.bigrams[bigram] = 1
             self.bigram_count += 1
 
-    def mono_frequancy(self):
-        self.mono_freq = self.monograms
-        for char in self.monograms:
-                p = self.monograms[char] / self.monogram_count
-                self.mono_freq[char] = p * log2(p)
+    def mono_frequancy_calc(self):
+        self.monogram_frequancy = self.monograms
+        for char in self.monogram_frequancy:
+                self.monograms[char] = self.monograms[char] / self.monogram_count
+
+    def bigr_frequancy_calc(self):
+        self.bigrams_frequancy = self.bigrams
+        for char in self.bigrams_frequancy:
+                self.bigrams[char] = self.bigrams[char] / self.bigram_count
+
+    def entrophy_calc(self, freq_list):
+        arr = freq_list
+        for char in arr:
+                arr[char] = arr[char] * log2(arr[char])
+        return -sum(arr.values())
 
 def main():
-    text_with_space = entrho_calc("text.txt")
+    # print("######spaces######")
+    # text_with_space = entrho_calc("text.txt")
+
+    # text_with_space.count_monograms()
+    # text_with_space.mono_frequancy_calc()
+    # print("mono freq:")
+    # for char, gram in text_with_space.monogram_frequancy.items():
+    #     print(f"{char}, {gram:.20f}")
+    # print(f"mono entrophy: {text_with_space.entrophy_calc(text_with_space.monogram_frequancy)}")
+
+    # text_with_space.count_bigrams()
+    # text_with_space.bigr_frequancy_calc()
+    # print("bigr freq:")
+    # for char, gram in text_with_space.bigrams_frequancy.items():
+    #     print(f"{char}, {gram:.20f}")
+    # print(f"bigr entrophy: {text_with_space.entrophy_calc(text_with_space.bigrams_frequancy)}")
+
+
+    print("######NO spaces######")
     text_without_space = entrho_calc("text.txt", False)
+    text_without_space.save_to_file("shit")
 
-    text_with_space.count_monograms()
-    text_with_space.count_bigrams()
+    text_without_space.count_monograms()
+    text_without_space.mono_frequancy_calc()
+    print("mono freq:")
+    for char, gram in text_without_space.monogram_frequancy.items():
+        print(f"{char}, {gram:.20f}")
+    print(f"mono entrophy: {text_without_space.entrophy_calc(text_without_space.monogram_frequancy)}")
 
-    text_with_space.mono_frequancy()
-    print("Monogram Frequencies with Spaces:")
-    for char, freq in text_with_space.mono_freq.items():
-        print(f"'{char}': {freq:.6f}")
-
-
+    text_without_space.count_bigrams()
+    text_without_space.bigr_frequancy_calc()
+    print("bigr freq:")
+    for char, gram in text_without_space.bigrams_frequancy.items():
+        print(f"{char}, {gram:.20f}")
+    print(f"bigr entrophy: {text_without_space.entrophy_calc(text_without_space.bigrams_frequancy)}")
 
 
 
