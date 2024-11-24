854d26df5596321f516ce03b9168360f16f13b8d
Zaptoss <zaptosgg@gmail.com>
2024-11-24 01:32:40 +0000

    RSA cryptosystem implementation. Key exchange protocol based on RSA implementation. TODO: Wrap by class to simplify usage, write comments, write tests (interact with api)

---
+++ lab4/perebynos_fb-22_vlasenko_fb-22_cp4/primes.py
@@ -0,0 +1,38 @@
+from primality_tests import trial_division_test, miller_rabin_primality_test
+from typing import Callable
+from random import randint
+
+k = 10
+trial_divisions_count = 512
+
+def _get_prime(bits_count: int, k: int, test_function: Callable[[int, int], bool]) -> int:
+    n0 = 1 << (bits_count - 1)
+    n1 = (1 << bits_count) - 1
+    while True:
+        x = randint(n0, n1)
+        if x % 2 == 0:
+            x += 1
+
+        for i in range((n1 - x) // 2):
+            p = x + 2*i
+
+            if not trial_division_test(p, trial_divisions_count):
+                continue
+
+            if test_function(p, k):
+                return p
+
+def get_prime(bits_count: int) -> int:
+    return _get_prime(bits_count, k, miller_rabin_primality_test)
+
+def _get_rsa_prime(bits_count: int, k: int, test_function: Callable[[int, int], bool]) -> int:
+    pp = _get_prime(bits_count-1, k, test_function)
+    while True:
+        p = 2*pp + 1
+        if test_function(p, k):
+            return p
+        
+        pp = _get_prime(bits_count-1, k, test_function)
+
+def get_rsa_prime(bits_count: int) -> int:
+    return _get_rsa_prime(bits_count, k, miller_rabin_primality_test)
